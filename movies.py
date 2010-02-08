ca4abd2b091c4c76524f84d5ebef80bac6443116
Tony Angerilli <tony@angerilli.ca>
2010-02-07 22:49:37 -0800

    Parses out the current date, and then creates a datetime for each showtime.

--- movies.py
+++ movies.py
@@ -3,31 +3,36 @@ import urllib2
 from pprint import pprint
 import re
 import logging
+import datetime
 
 def theater_html(theater_id, theater_name):
-    url = "http://www.cineplex.com/Theatres/TheatreDetails/%s/%s.aspx?date=2010-1-28" % (theater_id, theater_name)
+    url = "http://www.cineplex.com/Theatres/TheatreDetails/%s/%s.aspx" % (theater_id, theater_name)
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     result = response.read()
-    f = open("testdata2.html", "w")
-    result = f.write(result)
-    f.close()
+    # f = open("testdata3.html", "w")
+    # f.write(result)
+    # f.close()
     return result
     
 def cached_html(theater_id, theater_name):
-    f = open("testdata.html", "r")
+    f = open("data/testdata.html", "r")
     result = f.read()
     f.close()
     return result
     
 def get_showtimes(theater_id, theater_name):
-    soup = BeautifulSoup(cached_html(theater_id, theater_name))
+    soup = BeautifulSoup(theater_html(theater_id, theater_name))
     movielist = soup.findAll('div', "MovieList")
     movies = {}
     pat = re.compile("\d+:\d+\wm")
+    current_year = datetime.date.today().year
     for movie_div in movielist:
         try:
             title = movie_div.div.h1.a.string
+            movie_date_text_html = movie_div.div.findNextSibling('div').find('p', 'filmenhancement').contents[0].strip()
+            movie_date_text = "".join(movie_date_text_html.partition(str(current_year))[:2])
+            movie_date = datetime.datetime.strptime(movie_date_text, "%B %d, %Y").date()
             showtimes = movie_div.div.findNextSibling('div').find('p', 'showtimes')
             movies[title] = {"available":[], "soldout":[]}
             times = []
@@ -41,9 +46,13 @@ def get_showtimes(theater_id, theater_name):
                     times.append(thing.string)
             for time in times:
                 if "SOLD OUT" in time:
-                    movies[title]["soldout"].append(time.split(" ")[0])
+                    time = time.split(" ")[0]
+                    availability = "soldout"
                 else:
-                    movies[title]["available"].append(time)
+                    availability = "available"
+                movie_time = datetime.datetime.strptime(time, "%I:%M%p")
+                movie_time = movie_time.replace(movie_date.year, movie_date.month, movie_date.day)
+                movies[title][availability].append(movie_time)
         except Exception, e:
             logging.exception("Exception while parsing a movie")
     return movies
